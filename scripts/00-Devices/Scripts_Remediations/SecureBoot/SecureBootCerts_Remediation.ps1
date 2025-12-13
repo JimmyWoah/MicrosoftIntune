1d2b055b186b8b221eca0d76c9b18f6d6015c6cb
Simone Termine <153366789+JimmyWoah@users.noreply.github.com>
2025-12-13 13:40:19 +0100

    Delete scripts/00-Devices/Scripts_Remediations directory

--- scripts/00-Devices/Scripts_Remediations/SecureBoot/SecureBootCerts_Remediation.ps1
+++
@@ -1,9 +0,0 @@
-#0x5944 deploy all needed certificates and update to the PCA2023 signed Boot Manager
-#It requires reboot!
-
-$hivePath  = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\'
-$keyName  = 'AvailableUpdates'
-$keyValue = 0x5944
-
-if (!(Test-Path $hivePath)) {New-Item -Path $hivePath -Force}
-New-ItemProperty -Path $hivePath -Name $keyName -PropertyType DWord -Value $keyValue -Force
